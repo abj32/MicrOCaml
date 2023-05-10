ad6158ff0ba69fd90d2b490e512d2c14a105c1df
abj32 <abj32@terpmail.umd.edu>
2023-05-10 12:23:59 -0400

    Working Eval

--- _build/default/src/eval.ml
+++ _build/default/src/eval.ml
@@ -11,7 +11,7 @@ exception DivByZeroError
   You do not have to use these and you can modify them if you want. 
   If you do not use references, you will need the following data type:
 *)
-type values = Int of int|Bool of bool|String of string
+(*type values = Int of int|Bool of bool|String of string*)
 
 (* Adds mapping [x:v] to environment [env] *)
 let ref_extend env x v = (x, ref v)::env
@@ -23,7 +23,7 @@ let extend env x v = (x,v)::env
 let rec ref_lookup env x =
   match env with
   | [] -> raise (DeclareError ("Unbound variable " ^ x))
-  | (var, value)::t -> if x = var then !value else ref-lookup t x
+  | (var, value)::t -> if x = var then !value else ref_lookup t x
 
 let rec lookup env x = 
   match env with
@@ -38,7 +38,7 @@ let ref_extend_tmp env x = (x, ref (Int 0))::env
 let rec ref_update env x v =
   match env with
   | [] -> raise (DeclareError ("Unbound variable " ^ x))
-  | (var, value)::t -> if x = var then (value := v) else ref-update t x v
+  | (var, value)::t -> if x = var then (value := v) else ref_update t x v
         
 (* Removes the most recent variable,value binding from the environment *)
 let rec remove env x = match env with
@@ -49,11 +49,131 @@ let rec remove env x = match env with
 
 (* Evaluates MicroCaml expression [e] in environment [env],
    returning a value, or throwing an exception on error *)
-let rec eval_expr env e = failwith "unimplemented"
+let rec eval_expr env e =
+  match e with
+  | Value (value) -> value
+  | ID (id) -> ref_lookup env id
+  | Not (e) ->
+    let expr = eval_expr env e in
+    (match expr with
+    | Bool (b) -> Bool(not b)
+    | _ -> raise (TypeError "Not a boolean expression"))
+  | Binop (op, e1, e2) ->
+    (match op with
+    | Add ->
+      let e1' = eval_expr env e1 in
+      let e2' = eval_expr env e2 in
+      (match (e1', e2') with
+      | (Int (x1), Int (x2)) -> Int (x1 + x2)
+      | _ -> raise (TypeError "Not integers"))
+    | Sub ->
+      let e1' = eval_expr env e1 in
+      let e2' = eval_expr env e2 in
+      (match (e1', e2') with
+      | (Int (x1), Int (x2)) -> Int (x1 - x2)
+      | _ -> raise (TypeError "Not integers"))
+    | Mult ->
+      let e1' = eval_expr env e1 in
+      let e2' = eval_expr env e2 in
+      (match (e1', e2') with
+      | (Int (x1), Int (x2)) -> Int (x1 * x2)
+      | _ -> raise (TypeError "Not integers"))
+    | Div ->
+      let e1' = eval_expr env e1 in
+      let e2' = eval_expr env e2 in
+      (match (e1', e2') with
+      | (Int (x1), Int (x2)) -> if x2 <> 0 then Int (x1 / x2) else raise DivByZeroError
+      | _ -> raise (TypeError "Not integers"))
+    | Greater ->
+      let e1' = eval_expr env e1 in
+      let e2' = eval_expr env e2 in
+      (match (e1', e2') with
+      | (Int (x1), Int (x2)) -> Bool (x1 > x2)
+      | _ -> raise (TypeError "Not integers"))
+    | Less ->
+      let e1' = eval_expr env e1 in
+      let e2' = eval_expr env e2 in
+      (match (e1', e2') with
+      | (Int (x1), Int (x2)) -> Bool (x1 < x2)
+      | _ -> raise (TypeError "Not integers"))
+    | GreaterEqual ->
+      let e1' = eval_expr env e1 in
+      let e2' = eval_expr env e2 in
+      (match (e1', e2') with
+      | (Int (x1), Int (x2)) -> Bool (x1 >= x2)
+      | _ -> raise (TypeError "Not integers"))
+    | LessEqual ->
+      let e1' = eval_expr env e1 in
+      let e2' = eval_expr env e2 in
+      (match (e1', e2') with
+      | (Int (x1), Int (x2)) -> Bool (x1 <= x2)
+      | _ -> raise (TypeError "Not integers"))
+    | Concat ->
+      let e1' = eval_expr env e1 in
+      let e2' = eval_expr env e2 in
+      (match (e1', e2') with
+      | (String (s1), String (s2)) -> String (s1 ^ s2)
+      | _ -> raise (TypeError "Not strings"))
+    | Equal ->
+      let e1' = eval_expr env e1 in
+      let e2' = eval_expr env e2 in
+      (match (e1', e2') with
+      | (Int (x1), Int (x2)) -> Bool (x1 = x2)
+      | _ -> raise (TypeError "Not integers"))
+    | NotEqual ->
+      let e1' = eval_expr env e1 in
+      let e2' = eval_expr env e2 in
+      (match (e1', e2') with
+      | (Int (x1), Int (x2)) -> Bool (x1 <> x2)
+      | _ -> raise (TypeError "Not integers"))
+    | Or ->
+      let e1' = eval_expr env e1 in
+      let e2' = eval_expr env e2 in
+      (match (e1', e2') with
+      | (Bool (b1), Bool (b2)) -> Bool (b1 || b2)
+      | _ -> raise (TypeError "Not boolean expressions"))
+    | And ->
+      let e1' = eval_expr env e1 in
+      let e2' = eval_expr env e2 in
+      (match (e1', e2') with
+      | (Bool (b1), Bool (b2)) -> Bool (b1 && b2)
+      | _ -> raise (TypeError "Not boolean expressions")))
+  | If (guard, true_e, false_e) ->
+    let guard' = eval_expr env guard in
+    (match guard' with
+    | Bool (b) -> if b then eval_expr env true_e else eval_expr env false_e
+    | _ -> raise (TypeError "Not a boolean expression"))
+  | Let (var, b, e1, e2) ->
+    (match b with
+    | true ->
+      let env' = ref_extend_tmp env var in
+      let v = eval_expr env' e1 in
+      let _ = ref_update env' var v in
+      eval_expr env' e2
+    | false ->
+      let e1' = eval_expr env e1 in
+      let env' = ref_extend env var e1' in
+      eval_expr env' e2)
+  | Fun (var, e) -> Closure (env, var, e)
+  | FunctionCall (e1, e2) ->
+    let e1' = eval_expr env e1 in
+    (match e1' with
+    | Closure (a, x, e) ->
+      let e2' = eval_expr env e2 in
+      eval_expr (ref_extend a x e2') e
+    | _ -> raise (TypeError "No Closure"))
 
 (* Part 2: Evaluating mutop directive *)
 
 (* Evaluates MicroCaml mutop directive [m] in environment [env],
    returning a possibly updated environment paired with
    a value option; throws an exception on error *)
-let eval_mutop env m = failwith "unimplemented"
+let eval_mutop env m =
+  match m with
+  | Def (var, expr) ->
+    let env' = ref_extend_tmp env var in
+    let v = eval_expr env' expr in
+    let _ = ref_update env' var v in
+    (env', Some(eval_expr env' expr))
+  | Expr (expr) -> (env, Some (eval_expr env expr))
+  | NoOp -> (env, None)
--- _build/default/src/lexer.ml
+++ _build/default/src/lexer.ml
@@ -3,75 +3,87 @@ open TokenTypes
 (* PASTE YOUR LEXER FROM P4A HERE *)
 
 let tokenize input =
-  let rec tokenize_aux i = if i >= String.length input then [] else
-    match input.[i] with
-    | ' ' | '\t' | '\n' -> tokenize_aux (i + 1)
-    | '(' -> Tok_LParen :: tokenize_aux (i + 1)
-    | ')' -> Tok_RParen :: tokenize_aux (i + 1)
-    | '=' -> Tok_Equal :: tokenize_aux (i + 1)
-    | '<' ->
-      if i + 1 < String.length input then
-        match input.[i + 1] with
-        | '>' -> Tok_NotEqual :: tokenize_aux (i + 2)
-        | '=' -> Tok_LessEqual :: tokenize_aux (i + 2)
-        | '_' -> Tok_Less :: tokenize_aux (i + 1)
-    | '>' ->
-      if i + 1 < String.length input && input.[i + 1] = '=' then Tok_GreaterEqual :: tokenize_aux (i + 2)
-      else Tok_Greater :: tokenize_aux (i + 1)
-    | '|' ->
-      if i + 1 < String.length input && input.[i + 1] = '|' then Tok_Or :: tokenize_aux (i + 2)
-      else raise (InvalidInputException "Invalid")
-    | '&' ->
-      if i + 1 < String.length input && input.[i + 1] = '&' then Tok_Or :: tokenize_aux (i + 2)
-      else raise (InvalidInputException "Invalid")
-    | '+' -> Tok_Add :: tokenize_aux (i + 1)
-    | '-' -> 
-      if i + 1 < String.length input && input.[i + 1] = '>' then Tok_Arrow :: tokenize_aux (i + 2)
-      else Tok_Sub :: tokenize_aux (i + 1)
-    | '*' -> Tok_Mult :: tokenize_aux (i + 1)
-    | '/' -> Tok_Div :: tokenize_aux (i + 1)
-    | '^' -> Tok_Concat :: tokenize_aux (i + 1)
-    | ';' ->
-      if i + 1 < String.length input && input.[i + 1] = ';' then Tok_DoubleSemi :: tokenize_aux (i + 2)
-      else raise (InvalidInputException "Invalid")
-    | 'a'..'z' | 'A'..'Z' ->
-      let tokenize_aux_2 j = if j >= String.length input then [Tok_ID (String.sub input i (j-i))] else
-        match input.[j] with
-        | 'n' ->
-          if j + 2 < String.length input && input.[j + 1] = 'o' && input.[j + 2] = 't' then Tok_Not :: tokenize_aux (j + 3)
-          else tokenize_aux_2 (j + 1)
-        | 'i' ->
-          if j + 1 < String.length input && input.[j + 1] = 'f' then Tok_If :: tokenize_aux (j + 2)
-          else if j + 1 < String.length input && input.[j + 1] = 'n' then Tok_In :: tokenize_aux (j + 2)
-          else tokenize_aux_2 (j + 1)
-        | 't' ->
-          if j + 3 < String.length input && input.[j + 1] = 'h' && input.[j + 2] = 'e' && input.[j + 3] = 'n' then Tok_Then :: tokenize_aux (j + 4)
-          else if j + 3 < String.length input && input.[j + 1] = 'r' && input.[j + 2] = 'u' && input.[j + 3] = 'e' then Tok_Bool true :: tokenize_aux (j + 4)
-          else tokenize_aux_2 (j + 1)
-        | 'e' ->
-          if j + 3 < String.length input && input.[j + 1] = 'l' && input.[j + 2] = 's' && input.[j + 3] = 'e' then Tok_Else :: tokenize_aux (j + 4)
-          else tokenize_aux_2 (j + 1)
-        | 'l' ->
-          if j + 2 < String.length input && input.[j + 1] = 'e' && input.[j + 2] = 't' then Tok_Let :: tokenize_aux (j + 3)
-          else tokenize_aux_2 (j + 1)
-        | 'd' ->
-          if j + 2 < String.length input && input.[j + 1] = 'e' && input.[j + 2] = 'f' then Tok_Def :: tokenize_aux (j + 3)
-          else tokenize_aux_2 (j + 1)
-        | 'r' ->
-          if j + 2 < String.length input && input.[j + 1] = 'e' && input.[j + 2] = 'c' then Tok_Rec :: tokenize_aux (j + 3)
-          else tokenize_aux_2 (j + 1)
-        | 'f' ->
-          if j + 2 < String.length input && input.[j + 1] = "u" && input.[j + 2] = 'n' then Tok_Fun :: tokenize_aux (j + 3)
-          else if j + 4 < String.length input && input.[j + 1] = "a" && input.[j + 2] = 'l' && input.[j + 3] = 's' && input.[j + 4] = 'e' then Tok_Bool false :: tokenize_aux (j + 5)
-          else tokenize_aux_2 (j + 1)
-        | 'a'..'z' | 'A'..'Z' | '0'..'9' -> tokenize_aux_2 (j + 1)
-        | _ -> Tok_ID (String.sub input i (j - i)) :: tokenize_aux j
-      in tokenize_aux_2 if
-    | '0'..'9' ->
-      let rec tokenize_aux_2 j = if j >= String.length input then [Tok_Int (int_of_string (String.sub input i (j - i))] else
-        match input.[j] with
-        | '0'..'9' -> tokenize_aux_2 (j + 1)
-        | _ -> Tok_Int (int_of_string (String.sub input i (j - i))) :: tokenize_aux j
-      in tokenize_aux_2 i
-    | _ -> raise (InvalidInputException "Invalid")
-  in tokenize 0
+  let length = String.length input in
+
+  let rec tok pos =
+    if pos >= length then []
+
+    else if input.[pos] = ' ' || input.[pos] = '\t' || input.[pos] = '\n' then tok (pos + 1)
+    
+    else if Str.string_match (Str.regexp "(-?[0-9]+)") input pos then
+      let str = Str.matched_string input in
+      let token = String.sub str 1 ((String.length str) - 2) in
+      Tok_Int (int_of_string token) :: tok (pos + (String.length str))
+    
+    else if input.[pos] = '(' then Tok_LParen :: tok (pos + 1)
+    
+    else if input.[pos] = ')' then Tok_RParen :: tok (pos + 1)
+
+    else if input.[pos] = '=' then Tok_Equal :: tok (pos + 1)
+
+    else if Str.string_match (Str.regexp "<>") input pos then Tok_NotEqual :: tok (pos + 2)
+
+    else if Str.string_match (Str.regexp ">=") input pos then Tok_GreaterEqual :: tok (pos + 2)
+
+    else if input.[pos] = '>' then Tok_Greater :: tok (pos + 1)
+
+    else if Str.string_match (Str.regexp "<=") input pos then Tok_LessEqual :: tok (pos + 2)
+
+    else if input.[pos] = '<' then Tok_Less :: tok (pos + 1)
+    
+    else if Str.string_match (Str.regexp "||") input pos then Tok_Or :: tok (pos + 2)
+
+    else if Str.string_match (Str.regexp "&&") input pos then Tok_And :: tok (pos + 2)
+
+    else if Str.string_match (Str.regexp "not") input pos then Tok_Not :: tok (pos + 3)
+
+    else if Str.string_match (Str.regexp "if") input pos then Tok_If :: tok (pos + 2)
+
+    else if Str.string_match (Str.regexp "then") input pos then Tok_Then :: tok (pos + 4)
+
+    else if Str.string_match (Str.regexp "else") input pos then Tok_Else :: tok (pos + 4)
+
+    else if input.[pos] = '+' then Tok_Add :: tok (pos + 1)
+
+    else if Str.string_match (Str.regexp "->") input pos then Tok_Arrow :: tok (pos + 2)
+
+    else if input.[pos] = '-' then Tok_Sub :: tok (pos + 1)
+
+    else if input.[pos] = '*' then Tok_Mult :: tok (pos + 1)
+
+    else if input.[pos] = '/' then Tok_Div :: tok (pos + 1)
+
+    else if input.[pos] = '^' then Tok_Concat :: tok (pos + 1)
+
+    else if Str.string_match (Str.regexp "let") input pos then Tok_Let :: tok (pos + 3)
+
+    else if Str.string_match (Str.regexp "def") input pos then Tok_Def :: tok (pos + 3)
+
+    else if Str.string_match (Str.regexp "in") input pos then Tok_In :: tok (pos + 2)
+
+    else if Str.string_match (Str.regexp "rec") input pos then Tok_Rec :: tok (pos + 3)
+
+    else if Str.string_match (Str.regexp "fun") input pos then Tok_Fun :: tok (pos + 3)
+
+    else if Str.string_match (Str.regexp ";;") input pos then Tok_DoubleSemi :: tok (pos + 2)
+
+    else if Str.string_match (Str.regexp "true") input pos then Tok_Bool true :: tok (pos + 4)
+    
+    else if Str.string_match (Str.regexp "false") input pos then Tok_Bool false :: tok (pos + 5)
+
+    else if Str.string_match (Str.regexp "[0-9]+") input pos then
+      let token = Str.matched_string input in
+      Tok_Int (int_of_string token) :: tok (pos + (String.length token))
+
+    else if Str.string_match (Str.regexp "\"[^\"]*\"") input pos then
+      let str = Str.matched_string input in
+      let token = String.sub str 1 ((String.length str) - 2) in
+      Tok_String (token) :: tok (pos + (String.length str))
+
+    else if Str.string_match (Str.regexp "[a-zA-Z][a-zA-Z0-9]*") input pos then
+      let token = Str.matched_string input in
+      Tok_ID (token) :: tok (pos + (String.length token))
+
+    else raise (InvalidInputException "Invalid")
+
+  in tok 0
